bfe72909063717c1676260bfd485c885b5bdb276
Martijn Wiekens <14075611+martijnwiekens@users.noreply.github.com>
2023-02-24 17:20:55 +0000

    Add a way to easly test the parser

--- generator/__main__.py
+++ generator/__main__.py
@@ -2,3 +2,15 @@ from .generator import DocumentationGenerator
 
 dg = DocumentationGenerator()
 dg.generate()
+
+
+# Test mode
+# testUrls: list[str] = [
+#     "wx.functions.html",
+#     "wx.Window.html",
+# ]
+# for testUrl in testUrls:
+#     print("")
+#     print("Testing: " + testUrl)
+#     typingList = dg.parser.processClassApi(testUrl)
+#     print(typingList)
