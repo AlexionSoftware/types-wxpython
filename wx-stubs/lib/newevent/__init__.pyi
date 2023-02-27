0ab46a031486ed3471d2205b649683f27925f977
Martijn Wiekens <14075611+martijnwiekens@users.noreply.github.com>
2023-02-27 12:08:14 +0000

    Newly generated stubs

--- wx-stubs/lib/newevent/__init__.pyi
+++ wx-stubs/lib/newevent/__init__.pyi
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 from typing import Any, ContextManager, Optional, Union
 
-def NewCommandEvent() -> tuple['Event', int]:
+def NewCommandEvent() -> tuple[type['Event'], 'PyEventBinder']:
     """ Generates a new (command_event, binder) tuple.
 
         Source: https://docs.wxpython.org/wx.lib.newevent.html
     """
 
 
-def NewEvent() -> tuple['Event', int]:
+def NewEvent() -> tuple[type['Event'], 'PyEventBinder']:
     """ Generates a new (event, binder) tuple.
 
         Source: https://docs.wxpython.org/wx.lib.newevent.html
     """
+
+
